8567335d023a166e57663817365ed87760f39141
Hasan <ha03330224926@gmail.com>
2024-04-15 19:23:28 +0500

    Pending Proj

--- apps/server/src/graphQL/resolvers/auth.resolvers.ts
+++ apps/server/src/graphQL/resolvers/auth.resolvers.ts
@@ -3,9 +3,9 @@ import { Response, Request } from 'express';
 
 const authResolver = {
     Query: {
-        isAuth: async (root: any, args: any, context: any) => {
-            console.log("R", root);
-            console.log("args", args);
+        isAuth: async (_: any, args: any, context: any) => {
+            // console.log("R", _);  // Parent or Root Element which is undefined
+            // console.log("args", args);  // args comes from the req object of client side
 
             const CLIENT_URL = process.env.CLIENT_BASE_URL || '';
             const cont = context.authenticate('github', {scope: ["user: email"]});
